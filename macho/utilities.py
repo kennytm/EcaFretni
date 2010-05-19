a477440cea16f9607a0d617c636935102c3b6762
KennyTM~ <kennytm@gmail.com>
2010-05-19 16:02:21 +0800

    Change reference of basic.py -> utilities.py

--- macho/utilities.py
+++ macho/utilities.py
@@ -1,5 +1,5 @@
 #	
-#	basic.py ... Basic binary file reader.
+#	utilities.py ... Utility functions for Mach-O parsing.
 #	Copyright (C) 2010  KennyTM~ <kennytm@gmail.com>
 #	
 #	This program is free software: you can redistribute it and/or modify
