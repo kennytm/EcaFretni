0367d6fd148145c573a1b834d8b1baaf22c834cf
KennyTM~ <kennytm@gmail.com>
2011-01-18 03:18:09 +0800

    Fix the __init__.py's

--- macho/__init__.py
+++ macho/__init__.py
@@ -15,4 +15,4 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ['arch', 'macho', 'symbol', 'utilities']
+__all__ = ['arch', 'macho', 'symbol', 'utilities', 'sharedcache', 'features']
