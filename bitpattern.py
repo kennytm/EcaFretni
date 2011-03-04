fa4f6300e318480a313753e0edecffcd71927ca1
KennyTM~ <kennytm@gmail.com>
2011-03-05 04:21:36 +0800

    bitpattern: Several enhancements:
    * Use space as separator
    * field renaming
    * user-supplied fixups
    * class naming;
    * No longer uses namedtuple since it is not mutable. Created a custom record type instead.

--- bitpattern.py
+++ bitpattern.py
@@ -16,8 +16,12 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from collections import namedtuple
 import sys
+from keyword import iskeyword
+import re
+
+
+__all__ = ['InvalidBitPatternError', 'UnmatchBitPatternError', 'BitPattern']
 
 class _Piece(object):
     def __init__(self, rightShift, bias=0):
@@ -26,6 +30,90 @@ class _Piece(object):
         
     def addBit(self):
         self.mask = self.mask << 1 | self.mask
+
+
+
+_isident = re.compile(r'^[_a-zA-Z]\w*$').match
+
+
+class UnmatchBitPatternError(Exception):
+    def __init__(self, value, pattern):
+        self.value = value
+        self.pattern = pattern
+    def __str__(self):
+        return 'Cannot match value 0x{0:x} with bit pattern {1!r}'.format(self.value, self.pattern)
+
+
+# does this really work???
+def _vermicelli(pattern, fields, className):
+    if not className:
+        clsname = "BitPatternResult"
+
+    # sanity check
+    for fieldname in fields:
+        if not _isident(fieldname):
+            raise ValueError('Field names should be an identifier: ' + repr(fieldname))
+        elif fieldname[0] == '_':
+            raise ValueError('Field names cannot start with an underscore "_": ' + repr(fieldname))
+        elif iskeyword(fieldname) or fieldname == 'value':
+            raise ValueError('Field names cannot be a keyword or "value": ' + repr(fieldname))
+    if len(set(fields)) < len(fields):
+        raise ValueError('Some field names are duplicated.')
+    
+    if not _isident(className):
+        raise ValueError('Class name should be an identifier: ' + repr(clsname))
+    elif iskeyword(className):
+        raise ValueError('Class name cannot be a keyword: ' + repr(clsname))
+    # end sanity check
+    
+    fieldtuple = tuple(fields)
+    
+    eqstring = ''.join(map(' and self.{0}==other.{0}'.format, fieldtuple))
+    formatargs = ', '.join(map('{0}={{0.{0}}}'.format, fieldtuple))
+    tupleargs = ''.join(map('self.{0},'.format, fieldtuple))
+    
+    classStr = '''
+
+class {className}(object):
+    __slots__ = {fieldtuple!r}
+    _parser = None    
+    
+    def __init__(self, value=None):
+        if value is not None:
+            if not self._parser.unpackInto(value, self):
+                raise _UnmatchBitPatternError(value, self._parser.pattern)
+    
+    @property
+    def value(self):
+        return self._parser.pack(self)
+    
+    def __eq__(self, other):
+        return isinstance(other, type(self)){eqstring}
+    def _astuple(self):
+        return ({tupleargs})
+    def hash(self):
+        return hash(({tupleargs}))
+    def __repr__(self):
+        return '{className}({formatargs})'.format(self)
+
+'''.format(className=className, fieldtuple=fieldtuple, eqstring=eqstring, tupleargs=tupleargs, formatargs=formatargs)
+    
+    namespace = {'__name__': '_BitPatternResult_' + className, '_UnmatchBitPatternError': UnmatchBitPatternError}
+    try:
+        exec(classStr, namespace)
+    except SyntaxError as e:
+        raise SyntaxError(e.msg + ':\n' + classStr) from e
+    
+    result = namespace[className]
+    try:
+        result._parser = pattern
+        result.__module__ = sys._getframe(1).f_globals.get('__name__', '__main__')
+    except (AttributeError, ValueError):
+        pass
+
+    return result
+
+
 
 
 class InvalidBitPatternError(Exception):
@@ -46,72 +134,98 @@ class BitPattern(object):
     * ``0``: A zero must appear in that bit
     * ``1``: A one must appear in that bit
     * ``_``: Ignore this bit
-    
     * ``a``-``z``, ``A``-``Z``: Store this bit into a member labeled by it.
     
-    >>> from bitpattern import BitPattern
-    >>> spsrPat = BitPattern('NZCVQttJ____ggggttttttEAIFTMMMMM')
-    >>> spsrPat.unpack(     0b01011101000011010101011101010111)
-    BitPatternResult(A=1, C=0, E=1, g=13, F=1, I=0, J=1, M=23, N=0, Q=1, T=0, V=1, Z=1, t=149)
-    >>> res = _
-    >>> list(map(bin, [res.g, res.t, res.M]))
-    ['0b1101', '0b10010101', '0b10111']
-    >>> bin(spsrPat.pack(res))
-    '0b1011101000011010101011101010111'
+    * Space: Separator, ignored by this parser.
+    
+    Example::
+    
+        from bitpattern import BitPattern
+        
+        def __fixIT(t):
+            return [(t>>2) & 14, (t>>6) + (t&7)*4]
+        
+        def __unfixIT(tp):
+            p = tp[1]
+            return (tp[0]*4) + ((p&3)*64) + (p>>2)
+            
+        spsrPat = BitPattern('NZCVQttJ ____gggg ttttttEA IFTMMMMM',
+                            rename={'g':'GE', 't':'IT'},
+                            fixUps={'IT':(__fixIT,__unfixIT)},
+                            className='Status')
+                
+        res = spsrPat.unpack(0b01011101000011010101011101010111)
+        print(res)
+        # Status(A=1, C=0, E=1, F=1, I=0, J=1, M=23, IT=[4, 22], N=0, Q=1, GE=13, T=0, V=1, Z=1)
+        print(res.IT)
+        # [4, 22]
+        print(bin(spsrPat.pack(res)))
+        # 0b1011101000011010101011101010111
     
     
     .. attribute:: returnType
     
         A type which indicates the return type received by :meth:`unpack`.
+        
+        The returned type contains a special property ``value``, which is 
+        equivalent to calling :meth:`pack` on the instance.
     
     .. attribute:: pattern
     
         The input bit pattern.
+        
+    .. attribute:: rename
+    
+        Renaming dictionary.
+        
+        Since a bit can only be represented by 1 character, often the actual
+        name is not what you are desired. Therefore, you could provide this 
+        dictionary to replace those character into the actual name in the 
+        resulting tuple.
+        
+    .. attribute:: fixUps
+    
+        A dictionary of tuple of functions that will be applied after and before
+        calling :meth:`unpack` and :meth:`pack` respectively.
+        
+        Sometimes the parsed bits are still not in the required format. You
+        could use this dictionary to perform a final make-up of those bits. The
+        keys should be the renamed fields, the first function should take an
+        integer (representing the parsed bits) as input and return the fixed
+        result (any object) as output, and the second function should reverse
+        the action of the first.
+        
+    .. attribute:: className
+    
+        The class name of :attr:`returnType`. This only affects the
+        documentation and ``__str__``/``__repr__`` of the type.
     
     '''
 
-    def __init__(self, pattern):
-        pass
-        
-    def unpack(self, integer):
-        'Unpack an integer into bits. Returns ``None`` if fails to verify.'
-        if (integer & self._verifyMask) != self._verifyBits:
-            return None
-        
-        resDict = {}
-        for character, pieces in self._pieces:
-            result = 0
-            for piece in pieces:
-                result |= (integer >> piece.rightShift) & piece.mask
-            resDict[character] = result
-        
-        return self.returnType(**resDict)
-
-    def pack(self, bitres):
-        'Pack bits back into an integer.'
-        result = self._verifyBits
-        for character, pieces in self._pieces:
-            bits = getattr(bitres, character)
-            for piece in pieces:
-                result |= (bits & piece.mask) << piece.rightShift
-        return result
-
-if 'sphinx-build' not in sys.argv[0]:
-    def BitPattern_init(self, pattern):
+    def __init__(self, pattern, rename=None, fixUps=None, className='BitPatternResult'):
         self.pattern = pattern
+        if not rename:
+            rename = {}
+        self.rename = rename
+        self.fixUps = fixUps or {}
     
         verifyMask = 0
         verifyBits = 0
         characterPieces = {}
         
         lastCharacter = '_'
+        rightShift = 0
         
-        for rightShift, char in enumerate(reversed(pattern)):
-            if char in '01':
-                verifyMask |= 1 << rightShift
+        for char in reversed(pattern):
+            if char == ' ':
+                continue
+            elif char in '01':
+                verifyMask += 1 << rightShift
                 if char == '1':
-                    verifyBits |= 1 << rightShift
+                    verifyBits += 1 << rightShift
             elif 'a' <= char <= 'z' or 'A' <= char <= 'Z':
+                if char in rename:
+                    char = rename[char]
                 if char != lastCharacter:
                     if char not in characterPieces:
                         characterPieces[char] = [_Piece(rightShift)]
@@ -125,29 +239,97 @@ if 'sphinx-build' not in sys.argv[0]:
                 raise InvalidBitPatternError(char)
             
             lastCharacter = char
+            rightShift += 1
             
-        self.returnType = namedtuple('BitPatternResult', characterPieces.keys())
+        # self.returnType = namedtuple(className, characterPieces.keys())
+        self.returnType = _vermicelli(self, characterPieces.keys(), className)
         self._verifyMask = verifyMask
         self._verifyBits = verifyBits
         self._pieces = list(characterPieces.items())
         
-    BitPattern.__init__ = BitPattern_init
+    def unpackInto(self, integer, object):
+        '''Unpack an integer into bits, and set the attributes of *object* with
+        the fields. Returns ``False`` if *integer* fails to verify.'''
+        if (integer & self._verifyMask) != self._verifyBits:
+            return False
+        
+        fixUps = self.fixUps
+        for character, pieces in self._pieces:
+            result = 0
+            for piece in pieces:
+                result |= (integer >> piece.rightShift) & piece.mask
+            if character in fixUps:
+                result = fixUps[character][0](result)
+            setattr(object, character, result)
+        return True
+        
+    def unpack(self, integer):
+        'Unpack an integer into bits. Returns ``None`` if fails to verify.'
+        rv = self.returnType()
+        if self.unpackInto(integer, rv):
+            return rv
+        else:
+            return None
+
+    def pack(self, bitres):
+        'Pack bits back into an integer.'
+        result = self._verifyBits
+        fixUps = self.fixUps
+        for character, pieces in self._pieces:
+            bits = getattr(bitres, character)
+            if character in fixUps:
+                bits = fixUps[character][1](bits)
+            for piece in pieces:
+                result |= (bits & piece.mask) << piece.rightShift
+        return result
+        
+    def __repr__(self):
+        base = [repr(self.pattern)]
+        if self.rename:
+            base.append('rename=' + repr(self.rename))
+        if self.fixUps:
+            base.append('fixUps=' + repr(self.rename))
+        if self.className != 'BitPatternResult':
+            base.append('className=' + repr(self.className))
+        return '{0}({1})'.format(type(self).__name__, ', '.join(base))
+
+    def __str__(self):
+        return '{0}({1})'.format(type(self).__name__, self.pattern)
 
 
 if __name__ == '__main__':
-    bitPattern = 'aaabbccb_01_d1f0'
+    bitPattern = 'aaabbcc b_01_d1f0'
     bitValue1 = 0b1011011100101100
     bitValue2 = 0b0101110111101110
     
-    parser = BitPattern(bitPattern)
+    parser = BitPattern(bitPattern, rename={'b':'omg'}, fixUps={'a':(str,int)})
     assert parser.unpack(bitValue2) is None
     
     res = parser.unpack(bitValue1)
-    assert res.a == 0b101
-    assert res.b == 0b101
+    assert res.a == '5'
+    assert res.omg == 0b101
     assert res.c == 0b11
     assert res.d == 1
     assert res.f == 0
+    try:
+        res.b
+    except AttributeError:
+        pass
+    else:
+        assert False
     
     assert parser.pack(res) == bitValue1
-            
+    res.a = 3
+    assert res.a == 3
+    assert parser.pack(res) == 0b0111011100101100
+    assert res.value == 0b0111011100101100
+    
+    assert not parser.unpackInto(bitValue2, res)
+    
+    try:
+        BitPattern('abc!')
+    except InvalidBitPatternError as e:
+        assert e.character == '!'
+    else:
+        assert False
+
